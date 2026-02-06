1e568a8f53a886d7a0fecf21a28714102f6767b9
Nikhil <niktemporary69@gmail.com>
2026-02-06 20:45:19 +0530

    build a playlist video component with delay delete feature

--- frontend/src/components/playlist/PlaylistComponent.jsx
+++ frontend/src/components/playlist/PlaylistComponent.jsx
@@ -1,47 +1,58 @@
-import { useEffect, useState } from 'react'
-import { getUserPlaylists } from '../../api/playlist.api'
-import { useAuth } from '../../context/AuthContext.jsx'
-import {Button} from "../componentCollection.js"
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { getUserPlaylists } from "../../api/playlist.api";
+import { useAuth } from "../../context/AuthContext.jsx";
+import { Button } from "../componentCollection.js";
+import { Link } from "react-router-dom";
 
 export default function PlaylistComponent() {
-    const {user} = useAuth()
-    const [playlists, setPlaylists] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const { user } = useAuth();
+  const [playlists, setPlaylists] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect ( () => {
-        ( async () => {
-            try {
-                const res = await getUserPlaylists(user._id)
-                console.log(res.data.data);
-                setPlaylists(res.data.data);
-            } catch (error) {
-                console.error(error)
-            } finally {
-                setLoading(false);
-            }
-        })();
-    }, [user]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getUserPlaylists(user._id);
+        console.log(res.data.data);
+        setPlaylists(res.data.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [user]);
 
-  if (loading) return <div className="h-screen text-white text-4xl text-center">Loading</div>;
+  if (loading)
+    return (
+      <div className="h-screen text-white text-4xl text-center">Loading</div>
+    );
 
   return (
-    <div className='pt-16 h-screen'>
-        <div className='text-white text-2xl text-center m-4'>
-            Your Playlists
-        </div>
-        {playlists && playlists.map( (playlist) => 
-            <Link key={playlist._id} to={`/playlist/${playlist._id}`}>
-                <Button className='w-full text-center text-white hover:bg-gray-900' bgColor=''>
-                <div className='text-xl'>
-                    {playlist.name}
-                </div>
-                <div className='text-gray-200'>
-                    {playlist.description}
-                </div>
-            </Button>
+    <div className="pt-16 h-screen">
+      <div className="text-white text-2xl text-center m-4">Your Playlists</div>
+      {playlists &&
+        playlists.map((playlist) => (
+          <div
+            key={playlist._id}
+            className="text-center hover:bg-gray-900 mb-6"
+          >
+            <Link to={`/playlist/${playlist._id}`}>
+              <Button className="w-full text-center text-white" bgColor="">
+                <div className="text-xl">{playlist.name}</div>
+                <div className="text-gray-200">{playlist.description}</div>
+              </Button>
             </Link>
-         )}
+            <div className="flex justify-center gap-4">
+              <Button className="text-white p-0 m-0" bgColor="">
+                Edit
+              </Button>
+              <Button className="text-white rounded-full" bgColor="bg-red-700">
+                delete
+              </Button>
+            </div>
+          </div>
+        ))}
     </div>
-  )
+  );
 }
--- frontend/src/components/playlist/PlaylistVideosComponent.jsx
+++ frontend/src/components/playlist/PlaylistVideosComponent.jsx
@@ -3,26 +3,59 @@ import { useParams } from "react-router-dom";
 import { getPlaylistById } from "../../api/playlist.api";
 import { Link } from "react-router-dom";
 import { getAgoTime, getVideoDuration } from "../../utils/time";
+import Button from "../Button";
+import { removeVideoFromPlaylist } from "../../api/playlist.api";
 
 function PlaylistVideosComponent() {
   const { playlistId } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [playlist, setPlaylist] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const [pendingDelete, setPendingDelete] = useState(false);
+  const [deleteTimeout, setDeleteTimeout] = useState(null);
+  const [countdown, setCountdown] = useState(3);
+
+  const handleRemoveVideo = async (videoId) => {
+    setCountdown(3);
+    try {
+      setPendingDelete(true);
+      const countdownInterval = setInterval(
+        () => setCountdown((prev) => prev - 1),
+        1000,
+      );
+      setVideos(videos.filter((video) => video._id != videoId));
+      const timeOut = setTimeout(async () => {
+        await removeVideoFromPlaylist(playlistId, videoId);
+        setPendingDelete(false);
+        clearInterval(countdownInterval);
+      }, 3000);
+      setDeleteTimeout(timeOut);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUndoDelete = () => {
+    clearTimeout(deleteTimeout);
+    setPendingDelete(false);
+    clearInterval(countdownInterval);
+    setVideos(playlist.videos)
+  };
 
   useEffect(() => {
     (async () => {
       try {
-        console.log(playlistId);
         const res = await getPlaylistById(playlistId);
         setPlaylist(res.data.data[0]);
+        setVideos(res.data.data[0].videos);
       } catch (error) {
         console.error(error);
       } finally {
         setLoading(false);
       }
     })();
-  }, []);
+  }, [playlistId]);
 
   if (loading)
     return (
@@ -31,44 +64,80 @@ function PlaylistVideosComponent() {
       </div>
     );
   return (
-    <div className="pt-16 h-full text-center">
+    <div className="pt-16 h-full text-center bg-black">
       <div className="text-white text-3xl">{playlist.name}</div>
-      <div className="text-gray-200">{playlist.description}</div>
-      {playlist.videos &&
-        playlist.videos.map((video) => (
-          <Link key={video._id} to={`/watch/${video._id}/${playlistId}`}>
-            <div className="flex gap-12 m-8 hover:bg-gray-900 p-2">
-              <div className=" h-48 w-80">
-                <img
-                  src={video.thumbnail}
-                  alt={`${video.title} thumbnail image `}
-                  className="w-full h-full object-cover rounded-xl"
-                />
-              </div>
-              <div className="text-left">
-                <div className="text-white text-3xl mb-1">{video.title}</div>
-                <div className="text-gray-300 mb-4">{video.description}</div>
-                <div className="flex gap-2 mb-4">
+      <div className="text-gray-200 mb-2">{playlist.description}</div>
+      <div className="flex gap-4 justify-center">
+        <div className="text-sm text-gray-400 hover:text-red-600 cursor-pointer">
+          Edit
+        </div>
+        <div className="text-sm text-gray-400 hover:text-red-600 cursor-pointer">
+          Delete Playlist
+        </div>
+      </div>
+      {videos &&
+        videos.map((video) => (
+          <div
+            key={video._id}
+            className="flex justify-between hover:bg-gray-900"
+          >
+            <Link to={`/watch/${video._id}/${playlistId}`}>
+              <div className="flex gap-12 m-8 p-2">
+                <div className=" h-48 w-80">
+                  <img
+                    src={video.thumbnail}
+                    alt={`${video.title} thumbnail image `}
+                    className="w-full h-full object-cover rounded-xl"
+                  />
+                </div>
+                <div className="text-left">
+                  <div className="text-white text-3xl mb-1">{video.title}</div>
+                  <div className="text-gray-300 mb-4">{video.description}</div>
+                  <div className="flex gap-2 mb-4">
                     <div className="w-5">
-                        <img src={video.owner.avatar} alt="" srcset="" className="rounded-full"/>
+                      <img
+                        src={video.owner.avatar}
+                        alt=""
+                        srcset=""
+                        className="rounded-full"
+                      />
                     </div>
                     <div className="text-gray-400 text-sm">
-                        {video.owner.username}
+                      {video.owner.username}
                     </div>
                     <div className="text-gray-500 text-sm pl-6">
-                    {
-                        `${getAgoTime(video.createdAt)}`
-                    }
-                </div>
-                </div>
-                <div className="text-gray-200">
+                      {`${getAgoTime(video.createdAt)}`}
+                    </div>
+                  </div>
+                  <div className="text-gray-200">
                     {`${getVideoDuration(video.duration)}`}
+                  </div>
                 </div>
-                
               </div>
+            </Link>
+            <div className="pt-6 m-2">
+              <Button
+                className="rounded-full cursor-pointer"
+                onClick={() => handleRemoveVideo(video._id)}
+              >
+                delete
+              </Button>
             </div>
-          </Link>
+          </div>
         ))}
+      {pendingDelete && (
+        <div className="flex fixed bottom-4 left-2/5 z-50 bg-gray-800 p-3 rounded-full">
+          <div className="pt-2 text-white mr-4 ml-4">Video Deleted</div>
+          <Button
+            bgColor=""
+            className="border rounded-full hover:bg-red-600 cursor-pointer"
+            onClick={handleUndoDelete}
+          >
+            Undo
+          </Button>
+          <div className="pt-2 text-red-400 mr-4 ml-4">{`${countdown}s`}</div>
+        </div>
+      )}
     </div>
   );
 }
